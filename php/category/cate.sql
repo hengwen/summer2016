3dbcb3a128100754eb2a5be3d8ae6c99939e82ba
hengwen <hengweno@163.com>
2016-08-14 13:51:17 +0800

    全路径实现无限级分类

--- php/category/cate.sql
+++ php/category/cate.sql
@@ -2,6 +2,7 @@ create database if not exists imooc default charset utf8;
 
 use imooc;
 
+## 父id实现无限级分类
 create table if not exists category_parent(
 	id int(10) unsigned not null auto_increment,
     pId int(10) unsigned not null comment "父id",
@@ -21,3 +22,39 @@ values(1,0,'新闻',unix_timestamp()),
     (8,2,'风景图片',unix_timestamp()),
     (9,7,'日韩明星',unix_timestamp()),
     (10,9,'孙艺珍',unix_timestamp());
+    
+    
+    
+## 全路径方式实现无限极分类   
+create table if not exists category_path(
+	id int(10) unsigned not null auto_increment,
+    cateName varchar(30) not null,
+    path varchar(50) not null,
+    createTime int(10) not null,
+    primary key(id)
+    )engine=InnoDB default charset=utf8;
+    
+    
+insert into category_path(id,cateName,path,createTime) 
+values (1,'手机','',unix_timestamp()),
+	(2,'功能手机','1',unix_timestamp()),
+    (3,'老人手机','1,2',unix_timestamp()),
+    (4,'儿童手机','1,2',unix_timestamp()),
+    (5,'智能手机','1',unix_timestamp()),
+    (6,'android手机','1,5',unix_timestamp()),
+    (7,'IOS手机','1,5',unix_timestamp()),
+    (8,'WinPhone手机','1,5',unix_timestamp()),
+    (9,'色盲手机','1,2,4',unix_timestamp()),
+    (10,'大字手机','1,2,3',unix_timestamp());
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
